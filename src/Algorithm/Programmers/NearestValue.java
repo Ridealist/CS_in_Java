d13de266269287882ecdf1bc145dd9737c8439e3
Ridealist <cjsn92@gmail.com>
2023-01-05 23:55:47 +0900

    TIL(refactor): 내 풀이 리팩터링, 다른 사람 풀이 공부

--- src/Algorithm/Programmers/NearestValue.java
+++ src/Algorithm/Programmers/NearestValue.java
@@ -21,6 +21,30 @@ public class NearestValue {
             }
             answer[i] = -1;
             counter.put(ch, i);
+            /*
+            // 중복 로직 제거 refactoring
+            if (counter.containsKey(ch)) {
+                answer[i] = i - counter.get(ch);
+            } else {
+                answer[i] = -1;
+            }
+            counter.put(ch, i);
+
+            -> getOrDefault 의 아이디어 유추 가능!
+             */
+        }
+        return answer;
+    }
+
+    // 다른 사람 풀이 공부
+    public int[] solutionTwo(String s) {
+        int[] answer = new int[s.length()];
+
+        Map<Character, Integer> counter = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            answer[i] = i - counter.getOrDefault(ch, (i + 1));
+            counter.put(ch, i);
         }
         return answer;
     }
